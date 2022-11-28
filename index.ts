b22b67c4830dd8c7406bdb3d9695c53ca18b3852
Sophie Atkins <atkins@prisma.io>
2022-11-28 18:07:01 +0100

    repro of https://github.com/prisma/prisma/issues/16326

--- index.ts
+++ index.ts
@@ -12,17 +12,23 @@ const prisma = new PrismaClient({
 // you can do middlewares on
 // prisma.$use
 
-const populate = async () => {
-  await prisma.a.create();
+const populate = async (value: bigint) => {
+  await prisma.b.create({ data: { value } });
 };
 
 async function test() {
-  const a = await prisma.a.findFirst();
-  console.log(a);
+  const b = await prisma.$queryRaw<
+    { value: bigint }[]
+  >`SELECT SUM(1::BIGINT) AS value`;
+  const x = await prisma.$queryRaw<
+    { value: bigint }[]
+  >`SELECT SUM(1::BIGINT)::BIGINT AS value`;
+  console.log(`b (not cast): ${b[0].value}`);
+  console.log(`x (cast): ${x[0].value}`);
 }
 
 async function main() {
-  await populate();
+  // [9223372036854775807n].map(v => populate(v));
   // console.log(process.env.DATABASE_URL);
   return test();
 }
