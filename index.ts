2510c5f1faa0ed2ff92256ba8cbc2bfcd85b557b
Sophie Atkins <atkins@prisma.io>
2024-03-28 15:00:24 +0100

    reproducing prisma/prisma#15598
    Unable to load Node-API Library from .../prisma_debug/node_modules/.prisma/client/libquery_engine-darwin-arm64.dylib.node, Library may be corrupt

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
-
 // you can do stuff in the client constructor
 // const prisma = new PrismaClient({
 //   __internal: { enginePath: '/Users/matthias/repos/work/prisma-engine/target/debug/query-engine' }
@@ -12,13 +8,48 @@ const prisma = new PrismaClient({
 // you can do middlewares on
 // prisma.$use
 
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+const caseInsensitiveField = "hello world";
+const id = "9df0f936-51d6-4c55-8e01-5144e588a8a1";
+
 const populate = async () => {
-  await prisma.a.create();
+  await prisma.test.createMany({
+    data: {
+      id,
+      caseInsensitiveField,
+    },
+  });
 };
 
 async function test() {
-  const a = await prisma.a.findFirst();
-  console.log(a);
+  // const list = [0, 0] as const;
+  // console.log("Case insensitive field", {
+  //   findFirst: await Promise.all(
+  //     list.map(() =>
+  //       prisma.test.findFirst({
+  //         where: { caseInsensitiveField: caseInsensitiveField.toUpperCase() },
+  //       })
+  //     )
+  //   ),
+  //   findUnique: await Promise.all(
+  //     list.map(() =>
+  //       prisma.test.findUnique({
+  //         where: { caseInsensitiveField: caseInsensitiveField.toUpperCase() },
+  //       })
+  //     )
+  //   ),
+  // });
+  // console.log("Normal field", {
+  //   findFirst: await Promise.all(
+  //     list.map(() => prisma.test.findFirst({ where: { id } }))
+  //   ),
+  //   findUnique: await Promise.all(
+  //     list.map(() => prisma.test.findUnique({ where: { id } }))
+  //   ),
+  // });
 }
 
 async function main() {
