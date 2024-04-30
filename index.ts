00987d5175b85d53fef4ce44a555d2440ff0f5e6
Sophie Atkins <atkins@prisma.io>
2024-04-30 18:42:46 +0200

    attempt prisma/prisma#23926

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, BaseUser } from "@prisma/client";
 import { validate } from "@prisma/prisma-schema-wasm";
 import { loadSchemaFiles } from "@prisma/schema-files-loader";
 
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({
+  adapter,
   log: ["query"],
 });
+
+// const prisma = new PrismaClient({
+//   log: ["query"],
+// });
 
 // you can do stuff in the client constructor
 // const prisma = new PrismaClient({
@@ -14,23 +26,40 @@ const prisma = new PrismaClient({
 // you can do middlewares on
 // prisma.$use
 
+const id = "123e4567-e89b-12d3-a456-426655440000";
+
 const populate = async () => {
-  await prisma.a.create({
+  await prisma.baseUser.create({
     data: {
-      id: 0,
+      id,
+      intId: 1,
     },
   });
 };
 
+const getUnsafe = async (id: bigint): Promise<BaseUser | null> => {
+  const query = {
+    where: { intId: id },
+    // include: { user: true },
+  };
+
+  const result = await prisma.baseUser.findUnique(query);
+  if (!result) {
+    return null;
+  }
+
+  return result;
+};
+
 async function test() {
-  const a = await prisma.a.findFirst();
-  console.log(a);
+  const u = getUnsafe(BigInt(1));
+  console.log(u);
 }
 
 async function main() {
   // console.log(process.env.DATABASE_URL);
 
-  // await populate();
+  await populate();
   return test();
 
   // const prismaSchema = await loadSchemaFiles("./prisma/schema");
